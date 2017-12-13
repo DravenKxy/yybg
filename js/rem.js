9b4726fe3a9a2f15a1f3f8ccad56a9119fc0f2f6
Dravenkxy <951944680@qq.com>
2017-12-13 17:28:41 +0800

    a

---
+++ js/rem.js
@@ -0,0 +1,35 @@
+// 当页面加载之后
+window.onload=function  () {
+	// 常量 const
+	const designWidh=750;
+	const fontSize= 100;
+	function resizeFont(){
+		// 变量 var
+		var CW = document.documentElement.clientWidth;
+		
+		var ratio = CW/designWidh;
+
+		var newFontSize=ratio<1?fontSize*ratio:100;
+
+		document.querySelector("html").style.fontSize= newFontSize + "px";
+		
+	}
+	// 执行resizeFont
+	resizeFont();
+	// 窗口大小改变时执行resizeFont
+	window.onresize= resizeFont;
+	window.addEventListener("orientation",resizeFont,false);
+
+            // 屏幕自动旋转得时候
+            //
+         // var swiper = new Swiper('.swiper-container', {
+         //        pagination: '.swiper-pagination',
+         //        nextButton: '.swiper-button-next',
+         //        prevButton: '.swiper-button-prev',
+         //        paginationClickable: true,
+         //        spaceBetween: 0,
+         //        centeredSlides: true,
+         //        autoplay: 2500,
+         //        autoplayDisableOnInteraction: false
+         //   });
+}
